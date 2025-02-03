836025bab9a4aca7747ae76a156ec9b7a9e8852c
Var-Guz <davidvarguz.7@unitec.edu>
2025-02-03 00:07:59 -0600

    clase add

---
+++ EjercicioClase/SuperPoder.cs
@@ -0,0 +1,28 @@
+﻿
+namespace EjercicioClase
+{
+    public class SuperPoder
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Nivel { get; set; }
+
+        public SuperPoder(string nombre, string descripcion, int nivel)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Nivel = nivel;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Descripcion : {Descripcion}");
+            Console.WriteLine($"Nivel: {Nivel}");
+       
+
+            Console.WriteLine();
+        }
+
+    }
+}
