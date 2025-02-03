70cdbb5c125a7a72b68f319907754a9647982f16
Var-Guz <davidvarguz.7@unitec.edu>
2025-02-03 00:08:05 -0600

    case add

---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,32 @@
+﻿
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder SuperPoder { get; set; }
+
+        public SuperHeroe(string nombre, string identidadSecreta, string ciudad, bool puedeVolar, SuperPoder superPoder)
+        {
+            Nombre = nombre;
+            IdentidadSecreta = identidadSecreta;
+            Ciudad = ciudad;
+            PuedeVolar = puedeVolar;
+            SuperPoder = superPoder;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede Volar: {PuedeVolar}");
+            
+            Console.WriteLine();
+        }
+
+    }
+}
